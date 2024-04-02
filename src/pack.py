7a34d68051dc5a8c4b926d1af08d7a323ed81f13
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-04-02 00:03:20 -0500

    Add name number and cost to pack class

--- src/pack.py
+++ src/pack.py
@@ -1,5 +1,8 @@
 class Pack:
-    def __init__(self) -> None:
+    def __init__(self, name, number, cost) -> None:
+        self.name = name
+        self.number = number
+        self.cost = cost
         self.commons = []
         self.rares = []
         self.super_rares = []
